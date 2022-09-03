54ce15ac16821bcf285c30017b639512005a174d
mhufflep <mhufflep@student.21-school.ru>
2022-09-03 22:21:30 +0500

    script tester updated

--- tester/script.sh
+++ tester/script.sh
@@ -4,14 +4,15 @@ CXX=clang++
 CYCLES=1000
 # DEBUG= -g
 DEBUG=
-FT_ROOT_FOLDER="./"
+INCLUDE_DIR="./"
 TESTER_STD="tester_std"
 TESTER_FT="tester_ft"
-OUTPUT_STD_NAME="output_std"
-OUTPUT_FT_NAME="output_ft"
+OUTPUT_STD="output_std"
+OUTPUT_FT="output_ft"
+CONT_SRCS=
  
-${CXX} *.cpp -I ${FT_ROOT_FOLDER} ${DEBUG} -std=c++98 -o ${TESTER_FT}
-${CXX} *.cpp -I ${FT_ROOT_FOLDER} ${DEBUG} -std=c++98 -o ${TESTER_STD} -D _CONTAINER_TESTER_STD
+${CXX} *.cpp ${CONT_SRCS} -I ${INCLUDE_DIR} ${DEBUG} -std=c++98 -o ${TESTER_FT}
+${CXX} *.cpp  -D _CONTAINER_TESTER_STD      ${DEBUG} -std=c++98 -o ${TESTER_STD}
 
 ./${TESTER_STD} ${CYCLES} > ${OUTPUT_STD_NAME}
 ./${TESTER_FT} ${CYCLES} > ${OUTPUT_FT_NAME}
@@ -20,8 +21,8 @@ DIFF=$(diff ${OUTPUT_FT_NAME} ${OUTPUT_STD_NAME})
 if [ "$DIFF" == "" ]
 then
     echo "OK, all tests passed"
-    rm -f ${OUTPUT_FT_NAME} ${OUTPUT_STD_NAME} 
-    rm -f ${TESTER_FT} ${TESTER_STD}
+    rm -rf ${OUTPUT_FT} ${OUTPUT_STD} 
+    rm -rf ${TESTER_FT}* ${TESTER_STD}*
 else   
     echo "KO, some tests failed"
     echo "Try to compare ${OUTPUT_FT_NAME} and ${OUTPUT_STD_NAME}"
