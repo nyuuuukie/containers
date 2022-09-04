4b7475ab17fa06f2b7a5a028c93e5b716c78c7b3
mhufflep <mhufflep@student.21-school.ru>
2022-09-04 15:46:16 +0500

    visualizer updated

--- include/rbt_visualizer.hpp
+++ include/rbt_visualizer.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+// It is NOT a part of containers, 
+// just a useful tool that helps understand red-black trees.
+
 #include <iostream>
 
 #include "rbt.hpp"
@@ -18,14 +21,18 @@ private:
 	std::string	_red;
 	std::string	_black;
 	std::string	_reset;
+	std::string	_full_reset;
 	char _space;
 	char _hline;
 	char _vline;
+	char _continue;
 
 	void init(void);
 
 	template <typename T>
 	void visualize_lvl(rbt_node<T> *node, int lvl, bool lchild = false);
+
+	std::string center(std::string title, bool lchild = false);
 
 public:
 	template <typename T, typename C, typename A>
@@ -39,15 +46,13 @@ public:
 };
 
 std::string 
-center(std::string title, int width, bool lchild = false) {
-	
-	char c = ' ';
+rbt_visualizer::center(std::string title, bool lchild) {
 
-	int spaces = width - title.length();
+	int spaces = _data_size - title.length();
 	spaces = spaces < 0 ? 0 : spaces;
 
-	std::string right(spaces / 2 + (lchild * (spaces % 2)), c);
-	std::string left(spaces / 2 + (!lchild * (spaces % 2)), c);
+	std::string right(spaces / 2 + (lchild * (spaces % 2)), _space);
+	std::string left(spaces / 2 + (!lchild * (spaces % 2)), _space);
 
 	return (left + title + right);
 }
@@ -59,9 +64,11 @@ rbt_visualizer::init(void) {
 	_red = "\033[48;5;196;38;5;232m";
 	_black = "\033[48;5;232;38;5;231m";
 	_reset = "\033[38;5;220;49m";
+	_full_reset = "\033[39;49m";
 	_space = ' ';
 	_hline = '-';
 	_vline = '|';
+	_continue = '~';
 }
 
 template <typename T, typename C, typename A>
@@ -93,7 +100,7 @@ rbt_visualizer::visualize(ft::rbt<T, C, A> &tree) {
 		std::cout << std::endl << _line << std::endl;
 		_line.clear();
     }
-	std::cout << "\033[39;49m";
+	std::cout << _full_reset;
 }
 
 
@@ -105,7 +112,7 @@ rbt_visualizer::visualize_lvl(rbt_node<T> *node, int lvl, bool lchild) {
 		if (is_leaf_node(node)) {
 
 			std::string data = (_data_size < 4 ? "N" : "NULL");
-			data = center(data, _data_size, lchild);
+			data = center(data, lchild);
 			
 			std::cout << std::string(_space_block, _space);
 			std::cout << _black;
@@ -117,7 +124,6 @@ rbt_visualizer::visualize_lvl(rbt_node<T> *node, int lvl, bool lchild) {
 			_line.append(_space_block, _space);
 
 		} else {
-
 			const int space_count = _space_block / 2;
 			const int lines_count = _space_block - space_count;
 			
@@ -126,9 +132,9 @@ rbt_visualizer::visualize_lvl(rbt_node<T> *node, int lvl, bool lchild) {
 			std::string data = ss.str();
 			if (data.length() > _data_size) {
 				data.erase(_data_size - 1);
-				data.append(1, '~');
+				data.append(1, _continue);
 			}
-			data = center(data, _data_size, lchild);
+			data = center(data, lchild);
 
 			std::cout << std::string(space_count, _space);
 			std::cout << std::string(lines_count, _hline);
